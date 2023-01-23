15eb7d800cc0d8a74742e1f43a2ca8bbb4c23e2a
M.SaiF <M.SaiF@DESKTOP-OUIP6GO>
2023-01-23 19:38:51 +0200

    Add project files.

---
+++ Coree/Entities/Owner.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FillName { get; set; }
+        public string profil { get; set; }
+        public string Avatar { get; set; }
+
+        public Address Address { get; set; }
+    }
+
+
+
+
+
+
+
+}
